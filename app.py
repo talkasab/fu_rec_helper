72d8e74a12fb061d54da0b7c8b6f65e70c6f22f2
Tarik Alkasab <tarik.alkasab@gmail.com>
2024-08-01 15:35:39 -0400

    Adding a page on what kinds of recommendations are covered.

--- app.py
+++ app.py
@@ -55,6 +55,7 @@ st.image("images/csr_action_big_logo_horiz.png")
 
 about = st.Page("about.py", title="About", default=True)
 basic_usage = st.Page("basic_usage.py", title="Basic Workflow")
+covered_recs = st.Page("covered_recs.py", title="Covered Recommendations")
 
 vid_basic = st.Page("vid_basic.py", title="Basic Recommendation")
 vid_multiple = st.Page("vid_multiple.py", title="Multiple Recommendations")
@@ -68,7 +69,7 @@ recommendables = st.Page("recommendables.py", title="All Recommendable Exams")
 
 app = st.navigation(
     {
-        "About": [about, basic_usage],
+        "About": [about, basic_usage, covered_recs],
         "Videos": [
             vid_basic,
             vid_multiple,
